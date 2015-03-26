ec688065dbf5c6543f0b6472e063296262324b69
Josh Rolstad <jrolstad@gmail.com>
2015-03-25 18:10:13 -0700

    Adding tests around quality

--- src/GildedRose.Console/Program.cs
+++ src/GildedRose.Console/Program.cs
@@ -2,36 +2,44 @@
 
 namespace GildedRose.Console
 {
-    class Program
+    public class Program
     {
         IList<Item> Items;
+
+        public static IList<Item> UpdatedItems { get; private set; } 
+
         static void Main(string[] args)
         {
             System.Console.WriteLine("OMGHAI!");
 
-            var app = new Program()
-                          {
-                              Items = new List<Item>
-                                          {
-                                              new Item {Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20},
-                                              new Item {Name = "Aged Brie", SellIn = 2, Quality = 0},
-                                              new Item {Name = "Elixir of the Mongoose", SellIn = 5, Quality = 7},
-                                              new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80},
-                                              new Item
-                                                  {
-                                                      Name = "Backstage passes to a TAFKAL80ETC concert",
-                                                      SellIn = 15,
-                                                      Quality = 20
-                                                  },
-                                              new Item {Name = "Conjured Mana Cake", SellIn = 3, Quality = 6}
-                                          }
-
-                          };
-
-            app.UpdateQuality();
+            Run();
 
             System.Console.ReadKey();
 
+        }
+
+        public static void Run()
+        {
+            var app = new Program()
+            {
+                Items = new List<Item>
+                {
+                    new Item {Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20},
+                    new Item {Name = "Aged Brie", SellIn = 2, Quality = 0},
+                    new Item {Name = "Elixir of the Mongoose", SellIn = 5, Quality = 7},
+                    new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80},
+                    new Item
+                    {
+                        Name = "Backstage passes to a TAFKAL80ETC concert",
+                        SellIn = 15,
+                        Quality = 20
+                    },
+                    new Item {Name = "Conjured Mana Cake", SellIn = 3, Quality = 6}
+                }
+            };
+
+            app.UpdateQuality();
+            UpdatedItems = app.Items;
         }
 
         public void UpdateQuality()
---
+++ src/GildedRose.Tests/ProgramTests.cs
@@ -0,0 +1,46 @@
+﻿using System.Linq;
+using GildedRose.Console;
+using NUnit.Framework;
+
+namespace GildedRose.Tests
+{
+    [TestFixture]
+    public class ProgramTests
+    {
+        [Test]
+        public void Main_QualityIsNeverNegative()
+        {
+            // Arrange
+
+            // Act
+            Program.Run();
+
+            // Assert
+            var items = Program.UpdatedItems;
+
+            foreach (var item in items)
+            {
+                Assert.That(item.Quality,Is.GreaterThanOrEqualTo(0),"Quality for '{0}' is negative",item.Name);
+            }
+        }
+
+        [Test]
+        public void Main_QualityIsNeverMoreThanFifty()
+        {
+            // Arrange
+
+            // Act
+            Program.Run();
+
+            // Assert
+            var items = Program.UpdatedItems;
+
+            foreach (var item in items)
+            {
+                Assert.That(item.Quality, Is.LessThanOrEqualTo(50), "Quality for '{0}' is greater than 50", item.Name);
+            }
+        }
+
+        
+    }
+}
