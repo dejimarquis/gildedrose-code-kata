4827d330b877bbdb6b3d3acb35f114c573fbc71c
Josh Rolstad <jrolstad@gmail.com>
2015-03-25 18:41:01 -0700

    Updating so we can pass in the items

--- src/GildedRose.Console/Program.cs
+++ src/GildedRose.Console/Program.cs
@@ -21,22 +21,30 @@ namespace GildedRose.Console
 
         public static void Run()
         {
+            var items = new List<Item>
+            {
+                new Item {Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20},
+                new Item {Name = "Aged Brie", SellIn = 2, Quality = 0},
+                new Item {Name = "Elixir of the Mongoose", SellIn = 5, Quality = 7},
+                new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80},
+                new Item
+                {
+                    Name = "Backstage passes to a TAFKAL80ETC concert",
+                    SellIn = 15,
+                    Quality = 20
+                },
+                new Item {Name = "Conjured Mana Cake", SellIn = 3, Quality = 6}
+            };
+            
+            Run(items);
+        }
+
+        public static void Run(IList<Item> items)
+        {
             var app = new Program()
             {
-                Items = new List<Item>
-                {
-                    new Item {Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20},
-                    new Item {Name = "Aged Brie", SellIn = 2, Quality = 0},
-                    new Item {Name = "Elixir of the Mongoose", SellIn = 5, Quality = 7},
-                    new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80},
-                    new Item
-                    {
-                        Name = "Backstage passes to a TAFKAL80ETC concert",
-                        SellIn = 15,
-                        Quality = 20
-                    },
-                    new Item {Name = "Conjured Mana Cake", SellIn = 3, Quality = 6}
-                }
+                Items = items
+                
             };
 
             app.UpdateQuality();
