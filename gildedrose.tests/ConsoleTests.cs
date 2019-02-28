35fc1c3ab9ceef301138fcffff0d40df40836eb7
Josh Rolstad <jorolsta@microsoft.com>
2019-02-27 19:29:50 -0800

    Updating to use .net core

---
+++ gildedrose.tests/ConsoleTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace gildedrose.tests
+{
+    public class ConsoleTests
+    {
+        [Fact]
+        public void TestTheTruth()
+        {
+            Assert.True(true);
+        }
+    }
+}
